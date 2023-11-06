62d6fdc662b6cd9418c3caa317efb754a0b639cc
mesi <mesicharida202@gmail.com>
2023-11-06 23:52:59 +0900

    最初のコミット(頭文字C)

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { TaskModule } from './task/task.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TaskModule,
+    UserModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
---
+++ src/user/models/user.model.ts
@@ -0,0 +1,22 @@
+import { ObjectType, Field, Int, HideField } from '@nestjs/graphql';
+
+@ObjectType()
+export class User {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
+
+  @HideField()
+  password: string;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
+}
